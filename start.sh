54f353bed6ca941a2250dd7b33eab3f074255785
Arjun Raghavan <132340470+Kabalikabalo@users.noreply.github.com>
2025-02-14 21:59:47 +0000

    Fixed missing SpaCy model issue

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-uvicorn translator_api:app --host 0.0.0.0 --port $PORT --workers 1 --limit-concurrency 10
+pip install -r requirements.txt
+python -m spacy download fr_core_news_md  # Manually download the model
+uvicorn translator_api:app --host 0.0.0.0 --port $PORT
